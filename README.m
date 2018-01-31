5c48329830ba20b59bf8454e2e2b7b7d7b949dd7
gauthier-schweitzer <33396125+gauthier-schweitzer@users.noreply.github.com>
2018-01-31 16:58:43 +0100

    Update and rename README.md to README.m

--- README.m
+++ README.m
@@ -2,4 +2,4 @@
 
 This R code is an implementation of a method based on Lee, 2009 "Training, wages, and sample selection: Estimating sharp bounds on treatment effects"
 
-It simulates data of $\theta$
+It simulates data of a conventional setup "a la Heckman" and estimates lower and upper bounds to treatment effect using covariates.
